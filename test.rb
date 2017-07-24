83ee5737066ad067cdcc2c492450c3a0ea097060
lyx <26095970@qq.com>
2017-07-24 11:49:04 +0800

    gengxin yichang chuli

--- test.rb
+++ test.rb
@@ -8,6 +8,8 @@ require 'watir'
 
 #访问一次网站并记录访问时间
 def access_page(web_name)
+  @a = 0
+  @ZX_times += 1
   begin
     ieBrowser = WIN32OLE.new('InternetExplorer.Application')
     ieBrowser.Visible = true
@@ -20,16 +22,21 @@ def access_page(web_name)
     #ieBrowser.navigate(web_name)
     #sleep(5) until ieBrowser.busy == false
     plugin.GotoUrl(web_name)
+    #最多只等待30s
     control.Wait(plugin, 30)
   rescue
     puts "浏览器窗口打开异常"
-  #ensure
-   # plugin.Stop()
+    @YC_times += 1
+    @a += 1
+    #ensure
+    # plugin.Stop()
   end
+  puts @a
+  #此处加入异常处理当测试环境加载失败时直接返回nil，ruby在rescue里面无法return
+  return nil if @a == 1
   ip = plugin.log.Entries.Item(0).ServerIP
   puts "本次访问网页的IP是#{ip.chomp}"
   code = plugin.Log.Entries.Item(0).StatusCode
-  puts code
   if code == 200 or code == 301 or code == 302 or code == 303
     time_elapsed = plugin.Log.Entries.Summary.Time
     puts "本次访问网页花费的时间 = #{time_elapsed}"
@@ -38,7 +45,6 @@ def access_page(web_name)
     @fail_times += 1
   end
   ieBrowser.quit
-  @ZX_times += 1
   return time_elapsed
 end
 
@@ -46,26 +52,29 @@ end
 def access_times(web_name, times)
   @fail_times = 0
   @ZX_times = 0
+  @YC_times = 0
   time_array = []
   times.to_i.times do
     time_array << access_page(web_name)
-    @time_eclipsed = calculate_average(time_array)
-    @access_times = @ZX_times.to_i - @fail_times.to_i
-    puts "#################################"
+    #去除数组中值为nil的元素
+    @time_eclipsed = calculate_average(time_array.compact)
+    @access_times = @ZX_times.to_i - @fail_times.to_i - @YC_times.to_i
+    puts "##########################################"
     puts "#访问的网站是#{web_name.chomp}"
-    puts "#总共尝试访问#{@ZX_times}次,访问失败#{@fail_times}次    #"
-    puts "#成功访问网页#{@access_times}次的平均值是#{@time_eclipsed.round(3)} #"
-    puts "#################################"
+    puts "#总共尝试访问#{@ZX_times}次,环境加载异常#{@YC_times},访问失败#{@fail_times}次#"
+    puts "#成功访问网页#{@access_times.abs}次的平均值是#{@time_eclipsed.round(3)}          #"
+    puts "##########################################"
     sleep 5
   end
+  #将单个网站测试结果记录到D盘根目录
   txt_name1 = web_name.delete "://."
-  txt_name = txt_name1.chomp.insert(-1,'.txt')
+  txt_name = txt_name1.chomp.insert(-1, '.txt')
   aFile = File.new("D:\\#{txt_name}", "w")
-  aFile.puts "#################################"
-  aFile.puts "#访问的网站是#{web_name.chomp}"
-  aFile.puts "#总共尝试访问#{@ZX_times}次,访问失败#{@fail_times}次    #"
-  aFile.puts "#成功访问网页#{@access_times}次的平均值是#{@time_eclipsed.round(3)} #"
-  aFile.puts "#################################"
+  puts "##########################################"
+  puts "#访问的网站是#{web_name.chomp}"
+  puts "#总共尝试访问#{@ZX_times}次,环境加载异常#{@YC_times},访问失败#{@fail_times}次#"
+  puts "#成功访问网页#{@access_times.abs}次的平均值是#{@time_eclipsed.round(3)}          #"
+  puts "##########################################"
   aFile.close
 end
 
@@ -105,6 +114,7 @@ puts "#            wellcome           #"
 puts "#################################"
 puts "请输入执行次数"
 times =gets
+#读取配置文件C:\\name.txt，遍历文件中的域名
 File.open("C:\\name.txt") do |io|
   io.each_line do |line|
     access_times(line, times)
